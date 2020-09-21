ebbac00224833a184690639e711dd8aa5d80aef4
mf256 <44268440+mf256@users.noreply.github.com>
2020-09-21 18:52:47 +0200

    First connection to server.

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const socketio = require('socket.io');
 const cors = require('cors');
 
 const router = require('./router');
+const { Socket } = require('dgram');
 
 const app = express();
 const server = http.createServer(app);
@@ -11,6 +12,14 @@ const io = socketio(server);
 
 app.use(cors());
 app.use(router);
+
+io.on('connection', (socket) => {
+  console.log('User conected.');
+  
+  socket.on('disconnect', () => {
+    console.log('User disconected.');
+  });
+});
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server has started on ${PORT}.`));
--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
+import './Chat.module.scss';
 
-const Chat = () => (
-  <h1>Chat</h1>
-);
+const ENDPOINT = 'localhost:5000';
+
+let socket;
+
+const Chat = ({ location }) => {
+  // const [name, setName] = useState('');
+  // const [room, setRoom] = useState('');
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+    // setName(name);
+    // setRoom(room);
+
+    socket = io(ENDPOINT);
+    console.log(socket);
+  });
+
+  return (
+    <h1>Chat</h1>
+  );
+}
 
 export default Chat;
